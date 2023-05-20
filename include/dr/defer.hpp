16db5b9676d9ac40c0bc4b0f4cdf58890658cb36
David Reeves <reevdav@gmail.com>
2023-05-20 13:07:48 +0100

    Add defer construct

---
+++ include/dr/defer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+
+namespace dr
+{
+
+template <typename Func>
+struct Defer
+{
+    template <typename Func_>
+    Defer(Func_&& func) :
+        func_(std::forward<Func_>(func))
+    {
+    }
+
+    Defer(Defer const&) = delete;
+    Defer& operator=(Defer const&) = delete;
+
+    ~Defer() { func_(); }
+
+  private:
+    Func func_;
+};
+
+template <typename Func>
+Defer<Func> defer(Func&& func)
+{
+    return {std::forward<Func>(func)};
+}
+
+} // namespace dr
---
+++ test/defer_tests.cpp
@@ -0,0 +1,21 @@
+#include <utest.h>
+
+#include <dr/basic_types.hpp>
+#include <dr/defer.hpp>
+
+UTEST(defer, sanity)
+{
+    using namespace dr;
+
+    isize x = 0;
+
+    {
+        auto on_exit = defer([&]() {
+            ++x;
+        });
+
+        ASSERT_EQ(x, 0);
+    }
+
+    ASSERT_EQ(x, 1);
+}
