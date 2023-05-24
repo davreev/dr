5cf42542002bf2f0c02901dcdd9c727e921f7768
David Reeves <reevdav@gmail.com>
2023-05-24 10:17:05 +0100

    Fix ternary formatting

--- include/dr/math.hpp
+++ include/dr/math.hpp
@@ -5,6 +5,7 @@
 */
 
 #include <cmath>
+#include <type_traits>
 
 #include <dr/math_ctors.hpp>
 #include <dr/math_traits.hpp>
@@ -14,31 +15,31 @@ namespace dr
 {
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real pi = Real{3.141592653589793238462643383279502884197169399375105820974944592307816406L};
+inline constexpr Real pi{3.141592653589793238462643383279502884197169399375105820974944592307816406L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real inv_pi = Real{1.0} / pi<Real>;
+inline constexpr Real inv_pi{Real{1.0} / pi<Real>};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real e = Real{2.7182818284590452353602874713526624977572470936999595749669676277L};
+inline constexpr Real e{2.7182818284590452353602874713526624977572470936999595749669676277L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real sqrt2 = Real{1.4142135623730950488016887242096980785696718753769480731766797379L};
+inline constexpr Real sqrt2{1.4142135623730950488016887242096980785696718753769480731766797379L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real sqrt3 = Real{1.7320508075688772935274463415058723669428052538103806280558069794L};
+inline constexpr Real sqrt3{1.7320508075688772935274463415058723669428052538103806280558069794L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real ln2 = Real{0.6931471805599453094172321214581765680755001343602552541206800094L};
+inline constexpr Real ln2{0.6931471805599453094172321214581765680755001343602552541206800094L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real ln10 = Real{2.3025850929940456840179914546843642076011014886287729760333279009L};
+inline constexpr Real ln10{2.3025850929940456840179914546843642076011014886287729760333279009L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real log2e = Real{1.4426950408889634073599246810018921374266459541529859341354494069L};
+inline constexpr Real log2e{1.4426950408889634073599246810018921374266459541529859341354494069L};
 
 template <typename Real, std::enable_if_t<is_real<Real>>* = nullptr>
-inline constexpr Real log10e = Real{0.4342944819032518276511289189166050822943970058036665661144537831L};
+inline constexpr Real log10e{0.4342944819032518276511289189166050822943970058036665661144537831L};
 
 template <typename Real>
 constexpr Real deg_to_rad(Real const angle)
@@ -58,24 +59,21 @@ template <typename Scalar>
 constexpr Scalar sign(Scalar const x)
 {
     static_assert(is_real<Scalar> || is_integer<Scalar>);
-    return (x > Scalar{0}) ? Scalar{1} : (x < Scalar{0}) ? Scalar{-1} :
-                                                           Scalar{0};
+    return (x > Scalar{0}) ? Scalar{1} : ((x < Scalar{0}) ? Scalar{-1} : Scalar{0});
 }
 
 template <typename Real>
 constexpr Real saturate(Real const x)
 {
     static_assert(is_real<Real>);
-    return (x < Real{0.0}) ? Real{0.0} : (x > Real{1.0}) ? Real{1.0} :
-                                                           x;
+    return (x < Real{0.0}) ? Real{0.0} : ((x > Real{1.0}) ? Real{1.0} : x);
 }
 
 template <typename Scalar>
 constexpr Scalar clamp(Scalar const x, Scalar const min, Scalar const max)
 {
     static_assert(is_real<Scalar> || is_integer<Scalar>);
-    return (x < min) ? min : (x > max) ? max :
-                                         x;
+    return (x < min) ? min : ((x > max) ? max : x);
 }
 
 template <typename Scalar>
