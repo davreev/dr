9dacd026509ace34312c2989a80df9357dc10abb
David Reeves <reevdav@gmail.com>
2023-06-28 16:27:57 +0100

    Add option for pmr experimental shim

--- include/dr/shim/pmr/memory_resource.hpp
+++ include/dr/shim/pmr/memory_resource.hpp
@@ -1,18 +1,12 @@
 #pragma once
 
-// NOTE: C++17 isn't fully supported in the version of libc++ used by Emscripten (as of 3.1.13)
-// https://libcxx.llvm.org/Status/Cxx17.html
-#ifdef __EMSCRIPTEN__
-#define PMR_EXPERIMENTAL
-#endif
-
-#ifdef PMR_EXPERIMENTAL
+#ifdef DR_PMR_EXPERIMENTAL
 
 #include <experimental/memory_resource>
 
 namespace std
 {
-    namespace pmr = experimental::pmr;
+namespace pmr = experimental::pmr;
 }
 
 #else
