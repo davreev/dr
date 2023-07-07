19ce68e1be18321c366be8ca88be524ed161002d
David Reeves <reevdav@gmail.com>
2023-07-07 14:51:34 +0100

    Add pmr experimental CMake option

--- include/dr/shim/pmr/memory_resource.hpp
+++ include/dr/shim/pmr/memory_resource.hpp
@@ -2,6 +2,12 @@
 
 #ifdef DR_PMR_EXPERIMENTAL
 
+/*
+    NOTE: This is a workaround to the current lack of std::pmr namespace in libc++. It allows pmr
+    types to be used in way that's consistent with compliant C++17 standard library implementations.
+    See https://libcxx.llvm.org/Status/Cxx17.html for details on C++17 compliance in libc++.
+*/
+
 #include <experimental/memory_resource>
 
 namespace std
