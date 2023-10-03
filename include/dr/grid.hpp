fdd3f9a70126bde03cbc4b4574d2b39c976c2b8f
David Reeves <reevdav@gmail.com>
2023-10-03 12:50:13 +0100

    Refactor Grid type
    Extract free functions for mapping between index and id, rename member
    variables for clarity, and add tests.

--- include/dr/grid.hpp
+++ include/dr/grid.hpp
@@ -1,10 +1,42 @@
 #pragma once
 
-#include <dr/geometry_types.hpp>
 #include <dr/math_types.hpp>
 
 namespace dr
 {
+
+template <typename Index, int dim>
+Vec<Index, dim> grid_stride(Vec<Index, dim> const& count)
+{
+    Vec<Index, dim> result{};
+    result[0] = 1;
+
+    for (int i = 1; i < dim; ++i)
+        result[i] = count[i - 1] * result[i - 1];
+
+    return result;
+}
+
+template <typename Index, int dim>
+Index grid_to_index(Vec<Index, dim> const& id, Vec<Index, dim> const& stride)
+{
+    return id.dot(stride);
+}
+
+template <typename Index, int dim>
+Vec<Index, dim> index_to_grid(Index index, Vec<Index, dim> const& stride)
+{
+    Vec<Index, dim> result{};
+
+    for (int i = dim - 1; i > 0; --i)
+    {
+        result[i] = index / stride[i];
+        index -= result[i] * stride[i];
+    }
+    result[0] = index;
+
+    return result;
+}
 
 template <typename Scalar, int dim>
 struct Grid
@@ -12,48 +44,26 @@ struct Grid
     using Index = isize;
 
     Vec<Index, dim> count{};
-    Vec<Scalar, dim> scale{};
-    Vec<Scalar, dim> translate{};
+    Vec<Scalar, dim> spacing{Vec<Scalar, dim>::Ones()};
+    Vec<Scalar, dim> origin{};
 
-    Vec<Index, dim> stride() const
-    {
-        Vec<Index, dim> result{};
-        result[0] = 1;
-
-        for (int i = 1; i < dim; ++i)
-            result[i] = count[i - 1] * result[i - 1];
-
-        return result;
-    }
+    Vec<Index, dim> stride() const { return grid_stride(count); }
 
     Index to_index(Vec<Index, dim> const& grid_pt) const
     {
-        return grid_pt.dot(stride());
+        return grid_to_index(grid_pt, stride());
     }
 
-    Vec<Index, dim> to_grid(Index index) const
-    {
-        auto const stride = this->stride();
-        Vec<Index, dim> result{};
-
-        for (int i = dim - 1; i > 0; --i)
-        {
-            result[i] = index / stride[i];
-            index -= result[i] * stride[i];
-        }
-        result[0] = index;
-
-        return result;
-    }
+    Vec<Index, dim> to_grid(Index const index) const { return index_to_grid(index, stride()); }
 
     Vec<Scalar, dim> to_grid(Vec<Scalar, dim> const& world_pt) const
     {
-        return (world_pt - translate).array() / scale.array();
+        return (world_pt - origin).array() / spacing.array();
     }
 
     Vec<Scalar, dim> to_world(Vec<Scalar, dim> const& grid_pt) const
     {
-        return grid_pt.array() * scale.array() + translate.array();
+        return grid_pt.array() * spacing.array() + origin.array();
     }
 
     Vec<Scalar, dim> to_world(Vec<Index, dim> const& grid_pt) const
@@ -61,12 +71,9 @@ struct Grid
         return to_world(grid_pt.template cast<Scalar>().eval());
     }
 
-    Interval<Scalar, dim> bounds() const
+    Vec<Scalar, dim> const& extents() const
     {
-        return {
-            translate,
-            translate.array() + scale.array() * (count.array() - 1).template cast<Scalar>(),
-        };
+        return spacing.array() * (count.array() - 1).template cast<Scalar>();
     }
 };
 
---
+++ test/grid_tests.cpp
@@ -0,0 +1,166 @@
+#include <utest.h>
+
+#include <dr/grid.hpp>
+#include <dr/math_ctors.hpp>
+
+UTEST(grid, grid_to_index)
+{
+    using namespace dr;
+
+    struct TestCase
+    {
+        Vec3<isize> count;
+        Vec3<isize> id;
+        struct
+        {
+            isize index;
+        } expect;
+    };
+
+    TestCase const test_cases[] = {
+        {{4, 3, 2}, {0, 0, 0}, {0}},
+        {{4, 3, 2}, {1, 0, 0}, {1}},
+        {{4, 3, 2}, {0, 1, 0}, {4}},
+        {{4, 3, 2}, {0, 0, 1}, {12}},
+        {{4, 3, 2}, {1, 1, 0}, {5}},
+        {{4, 3, 2}, {1, 0, 1}, {13}},
+        {{4, 3, 2}, {0, 1, 1}, {16}},
+        {{4, 3, 2}, {1, 1, 1}, {17}},
+    };
+
+    for (auto const& [count, id, expect] : test_cases)
+    {
+        isize const index = grid_to_index(id, grid_stride(count));
+        ASSERT_EQ(expect.index, index);
+    }
+}
+
+UTEST(grid, index_to_grid)
+{
+    using namespace dr;
+
+    struct TestCase
+    {
+        Vec3<isize> count;
+        isize index;
+        struct
+        {
+            Vec3<isize> id;
+        } expect;
+    };
+
+    TestCase const test_cases[] = {
+        {{4, 3, 2}, 0, {{0, 0, 0}}},
+        {{4, 3, 2}, 1, {{1, 0, 0}}},
+        {{4, 3, 2}, 4, {{0, 1, 0}}},
+        {{4, 3, 2}, 12, {{0, 0, 1}}},
+        {{4, 3, 2}, 5, {{1, 1, 0}}},
+        {{4, 3, 2}, 13, {{1, 0, 1}}},
+        {{4, 3, 2}, 16, {{0, 1, 1}}},
+        {{4, 3, 2}, 17, {{1, 1, 1}}},
+    };
+
+    for (auto const& [count, index, expect] : test_cases)
+    {
+        Vec3<isize> const id = index_to_grid(index, grid_stride(count));
+        ASSERT_EQ(expect.id[0], id[0]);
+        ASSERT_EQ(expect.id[1], id[1]);
+        ASSERT_EQ(expect.id[2], id[2]);
+    }
+}
+
+UTEST(grid, grid_to_world)
+{
+    using namespace dr;
+
+    constexpr f64 eps = 1.0e-8;
+
+    struct TestCase
+    {
+        Grid3<f64> grid;
+        Vec3<f64> grid_pt;
+        struct
+        {
+            Vec3<f64> world_pt;
+        } expect;
+    };
+
+    TestCase const test_cases[] = {
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {0.0, 0.0, 0.0},
+            {{1.0, 1.0, 1.0}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {0.5, 0.5, 0.5},
+            {{6.0, 6.0, 6.0}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {1.0, 1.0, 1.0},
+            {{11.0, 11.0, 11.0}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {1.0, 0.5, 2.0},
+            {{11.0, 6.0, 21.0}},
+        },
+    };
+
+    for (auto const& [grid, grid_pt, expect] : test_cases)
+    {
+        Vec3<f64> const world_pt = grid.to_world(grid_pt);
+        ASSERT_NEAR(expect.world_pt[0], world_pt[0], eps);
+        ASSERT_NEAR(expect.world_pt[1], world_pt[1], eps);
+        ASSERT_NEAR(expect.world_pt[2], world_pt[2], eps);
+    }
+}
+
+UTEST(grid, world_to_grid)
+{
+    using namespace dr;
+
+    constexpr f64 eps = 1.0e-8;
+
+    struct TestCase
+    {
+        Grid3<f64> grid;
+        Vec3<f64> world_pt;
+        struct
+        {
+            Vec3<f64> grid_pt;
+        } expect;
+    };
+
+    TestCase const test_cases[] = {
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {1.0, 1.0, 1.0},
+            {{0.0, 0.0, 0.0}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {6.0, 6.0, 6.0},
+            {{0.5, 0.5, 0.5}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {11.0, 11.0, 11.0},
+            {{1.0, 1.0, 1.0}},
+        },
+        {
+            {{4, 3, 2}, vec<3>(10.0), vec<3>(1.0)},
+            {11.0, 6.0, 21.0},
+            {{1.0, 0.5, 2.0}},
+        },
+    };
+
+    for (auto const& [grid, world_pt, expect] : test_cases)
+    {
+        Vec3<f64> const grid_pt = grid.to_grid(world_pt);
+        ASSERT_NEAR(expect.grid_pt[0], grid_pt[0], eps);
+        ASSERT_NEAR(expect.grid_pt[1], grid_pt[1], eps);
+        ASSERT_NEAR(expect.grid_pt[2], grid_pt[2], eps);
+    }
+}
