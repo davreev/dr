75c5d00bb2551961aa83d14678e54c42728ec648
David Reeves <reevdav@gmail.com>
2023-12-01 00:23:10 +0000

    Remove unused hash funcs

--- include/dr/mesh_elements.hpp
+++ include/dr/mesh_elements.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <algorithm>
-
 #include <dr/container_utils.hpp>
 #include <dr/hash_map.hpp>
 #include <dr/linalg_reshape.hpp>
@@ -14,18 +12,6 @@ namespace dr
 
 struct ElementHash
 {
-    template <typename Index>
-    constexpr usize operator()(Span<Index const> const& indices) const
-    {
-        return hash(indices.data(), indices.size());
-    }
-
-    template <typename Index>
-    constexpr usize operator()(Span<Index> const& indices) const
-    {
-        return hash(indices.data(), indices.size());
-    }
-
     template <typename Index, int size>
     constexpr usize operator()(Vec<Index, size> const& indices) const
     {
@@ -33,17 +19,6 @@ struct ElementHash
     }
 
   private:
-    template <typename Index>
-    static constexpr usize hash(Index const* const indices, isize const size)
-    {
-        usize result{17};
-
-        for (isize i = 0; i < size; ++i)
-            result = 31 * result + static_cast<usize>(indices[i]);
-
-        return result;
-    }
-
     template <int size, typename Index>
     static constexpr usize hash(Index const* const indices)
     {
@@ -58,22 +33,10 @@ struct ElementHash
 
 struct ElementEqual
 {
-    template <typename Index>
-    bool operator()(Span<Index const> const& a, Span<Index const> const& b) const
-    {
-        return std::equal(begin(a), end(a), begin(b));
-    }
-
-    template <typename Index>
-    bool operator()(Span<Index> const& a, Span<Index> const& b) const
-    {
-        return std::equal(begin(a), end(a), begin(b));
-    }
-
     template <typename Index, int size>
     bool operator()(Vec<Index, size> const& a, Vec<Index, size> const& b) const
     {
-        return std::equal(a.data(), a.data() + size, b.data());
+        return a == b;
     }
 };
 
@@ -88,16 +51,15 @@ void make_vertex_to_edge(
     IncidenceMap<Index, 2>& result)
 {
     result.clear();
-    result.reserve(face_vertices.size() * 3); // Avoids further allocs if mesh is genus 0
+    result.reserve(face_vertices.size() * 3); // Accounts for 3 edges per face
     Index n_e = 0;
 
-    // Inserts a pair of directed edges between the given pair of vertices if they're not already
-    // connected
-    auto try_insert = [&](Vec2<Index> const& e_v, Index const e) -> bool {
+    // Inserts a pair of oriented edges between the given pair of vertices
+    auto const try_insert = [&](Vec2<Index> const& e_v, Index const e) -> bool {
         if (result.emplace(e_v, e).second)
         {
-            // Ensure oppositely oriented edges have consecutive indices
-            result[e_v.reverse()] = e + 1;
+            // Assign the next index to the oppositely oriented edge
+            result[e_v({1, 0})] = e + 1;
             return true;
         }
 
