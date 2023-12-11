e29861c31c388a32f8285455d86b69bb6f781e67
David Reeves <reevdav@gmail.com>
2023-12-11 10:40:46 +0000

    Rename Defer as Deferred

--- include/dr/defer.hpp
+++ include/dr/defer.hpp
@@ -6,18 +6,18 @@ namespace dr
 {
 
 template <typename Func>
-struct Defer
+struct Deferred
 {
     template <typename Func_>
-    Defer(Func_&& func) :
+    Deferred(Func_&& func) :
         func_(std::forward<Func_>(func))
     {
     }
 
-    Defer(Defer const&) = delete;
-    Defer& operator=(Defer const&) = delete;
+    Deferred(Deferred const&) = delete;
+    Deferred& operator=(Deferred const&) = delete;
 
-    ~Defer() { func_(); }
+    ~Deferred() { func_(); }
 
   private:
     Func func_;
@@ -25,7 +25,7 @@ struct Defer
 
 template <typename Func>
 [[nodiscard]]
-Defer<Func> defer(Func&& func)
+Deferred<Func> defer(Func&& func)
 {
     return {std::forward<Func>(func)};
 }
