8f301117c336123fd0b4dc0e2bf72c32607b901a
David Reeves <reevdav@gmail.com>
2024-08-09 10:35:23 +0100

    Add StableHashMap/Set aliases

--- include/dr/hash_map.hpp
+++ include/dr/hash_map.hpp
@@ -12,4 +12,11 @@ template <
     typename KeyEqual = std::equal_to<Key>>
 using HashMap = ankerl::unordered_dense::pmr::map<Key, Value, KeyHash, KeyEqual>;
 
+template <
+    typename Key,
+    typename Value,
+    typename KeyHash = Hash<Key>,
+    typename KeyEqual = std::equal_to<Key>>
+using StableHashMap = ankerl::unordered_dense::pmr::segmented_map<Key, Value, KeyHash, KeyEqual>;
+
 } // namespace dr
--- include/dr/hash_set.hpp
+++ include/dr/hash_set.hpp
@@ -11,4 +11,10 @@ template <
     typename KeyEqual = std::equal_to<Key>>
 using HashSet = ankerl::unordered_dense::pmr::set<Key, KeyHash, KeyEqual>;
 
+template <
+    typename Key,
+    typename KeyHash = Hash<Key>,
+    typename KeyEqual = std::equal_to<Key>>
+using StableHashSet = ankerl::unordered_dense::pmr::segmented_set<Key, KeyHash, KeyEqual>;
+
 } // namespace dr
