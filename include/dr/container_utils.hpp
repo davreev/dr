5ede6cdd33a797b1f04f1b7dd64e0906c4f72455
David Reeves <reevdav@gmail.com>
2024-09-04 00:45:31 +0100

    Remove redundant size_as impl

--- include/dr/container_utils.hpp
+++ include/dr/container_utils.hpp
@@ -4,6 +4,7 @@
     Assorted helper functions for container types
 */
 
+#include <dr/basic_types.hpp>
 #include <dr/container_traits.hpp>
 #include <dr/span.hpp>
 
@@ -12,23 +13,24 @@ namespace dr
 namespace impl
 {
 
+template <typename T = isize>
 struct Size
 {
     // NOTE: This is implemented as a function object to avoid ADL insanity
     // (https://quuxplusone.github.io/blog/2018/06/17/std-size/)
 
     /// Returns the number of elements in the given array
-    template <typename T, isize n>
-    constexpr isize operator()(T (& /*array*/)[n]) const
+    template <typename U, T n>
+    constexpr T operator()(U (& /*array*/)[n]) const
     {
         return n;
     }
 
     /// Returns the number of elements in the given container
     template <typename Container>
-    constexpr isize operator()(Container&& container) const
+    constexpr T operator()(Container&& container) const
     {
-        return static_cast<isize>(container.size());
+        return static_cast<T>(container.size());
     }
 };
 
@@ -36,19 +38,8 @@ struct Size
 
 inline constexpr impl::Size size{};
 
-/// Returns the number of elements in the given array using a specified size type
-template <typename Size, typename T, Size n>
-constexpr Size size_as(T const (& /*array*/)[n])
-{
-    return n;
-}
-
-/// Returns the number of elements in the given container using a specified size type
-template <typename Size, typename Container>
-constexpr Size size_as(Container&& container)
-{
-    return static_cast<Size>(container.size());
-}
+template <typename T>
+inline constexpr impl::Size<T> size_as{};
 
 /// Creates a span from the given pointer and size
 template <typename T>
