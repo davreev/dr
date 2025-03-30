b48ea8b858dfe241e4ca6fcbc94a8abe99d2decb
David Reeves <reevdav@gmail.com>
2025-03-30 11:52:52 +0100

    Add always_false constant

--- include/dr/constants.hpp
+++ include/dr/constants.hpp
@@ -8,4 +8,7 @@ namespace dr
 template <typename Index, std::enable_if_t<is_integer<Index> || is_natural<Index>>* = nullptr>
 inline constexpr Index invalid_index{~0};
 
+template <typename T>
+inline constexpr bool always_false{false};
+
 }
