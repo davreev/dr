270b47bbdeb91a4bf9992eb37ffae7f0467dfbb4
David Reeves <reevdav@gmail.com>
2025-03-30 12:01:08 +0100

    Rename linalg traits
    For consistency with math traits

--- include/dr/linalg_traits.hpp
+++ include/dr/linalg_traits.hpp
@@ -10,35 +10,35 @@ namespace impl
 {
 
 template <typename T>
-struct IsMat
+struct IsMatrix
 {
     static constexpr bool value{std::is_base_of_v<Eigen::MatrixBase<T>, T>};
 };
 
 template <typename T>
-struct IsVec
+struct IsVector
 {
-    static constexpr bool value{IsMat<T>::value && T::ColsAtCompileTime == 1};
+    static constexpr bool value{IsMatrix<T>::value && T::ColsAtCompileTime == 1};
 };
 
 template <typename T>
-struct IsCovec
+struct IsCovector
 {
-    static constexpr bool value{IsMat<T>::value && T::RowsAtCompileTime == 1};
+    static constexpr bool value{IsMatrix<T>::value && T::RowsAtCompileTime == 1};
 };
 
 } // namespace impl
 
 /// True if T is a matrix type
 template <typename T>
-inline constexpr bool is_mat = impl::IsMat<std::decay_t<T>>::value;
+inline constexpr bool is_matrix = impl::IsMatrix<std::decay_t<T>>::value;
 
 /// True if T is a matrix type with a single column
 template <typename T>
-inline constexpr bool is_vec = impl::IsVec<std::decay_t<T>>::value;
+inline constexpr bool is_vector = impl::IsVector<std::decay_t<T>>::value;
 
 /// True if T is a matrix type with a single row
 template <typename T>
-inline constexpr bool is_covec = impl::IsCovec<std::decay_t<T>>::value;
+inline constexpr bool is_covector = impl::IsCovector<std::decay_t<T>>::value;
 
 } // namespace dr
