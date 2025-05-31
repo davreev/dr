0de3dc186f21d3aec53183d005d5b7371b722d69
David Reeves <reevdav@gmail.com>
2025-06-01 00:58:19 +0100

    Remove Eigen hash

--- cmake/deps/eigen.cmake
+++ cmake/deps/eigen.cmake
@@ -7,7 +7,6 @@ include(FetchContent)
 FetchContent_Declare(
     eigen
     URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
-    URL_HASH SHA256=eba3f3d414d2f8cba2919c78ec6daab08fc71ba2ba4ae502b7e5d4d99fc02cda
 )
 
 FetchContent_GetProperties(eigen)
